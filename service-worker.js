c241f2761ed229d4469c26b97535391dfbb14758
MrPangu <235312546+mypangu@users.noreply.github.com>
2026-06-11 18:36:28 +0530

    Fresh Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Optimized Service Worker for Meow Streaming Site
 // service-worker.js
 
-const CACHE_VERSION = '3011250100';
+const CACHE_VERSION = '1106261836';
 const CACHE_NAMES = {
   STATIC: `meow-static-${CACHE_VERSION}`,
   DYNAMIC: `meow-dynamic-${CACHE_VERSION}`,
